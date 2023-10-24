b690bc43067e1233f8b6ec6c1fe1d6d0fa7875c3
helen_kim@progressive.com <helen_kim@progressive.com>
2023-10-24 16:52:45 -0400

    add calculator day 2 (Tuesday)

---
+++ stringcalculator-tuesday/StringCalculator.cs
@@ -0,0 +1,24 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculator
+{
+    string[] strArrNumbers;
+
+    public int Add(string numbers)
+    {
+        if (numbers == "")
+        {
+            return 0;
+        }
+        //if (numbers.Contains(','))
+        //{
+        return numbers.Split(',', '\n') //["1", "2", ...]
+            .Select(int.Parse) //[1,2,3,...] (works like mapping)
+            .Sum(); //sums up the array
+                    //language integrated query
+                    //}
+
+        //return int.Parse(numbers);
+    }
+}
---
+++ stringcalculator-tuesday/StringCalculatorTests.cs
@@ -0,0 +1,66 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculatorTests
+{
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("");
+
+        Assert.Equal(0, result);
+    }
+
+    [Theory]
+    [InlineData("1", 1)]
+    [InlineData("2", 2)]
+    [InlineData("3", 3)]
+    public void SingleDigit(string input, int expected)
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add(input);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("1", 1)]
+    [InlineData("2", 2)]
+    [InlineData("123", 123)]
+    public void NoDelimeterString(string input, int expected)
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add(input);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("1,1,1", 3)]
+    [InlineData("1,2,3,4,5", 15)]
+    [InlineData("2,2", 4)]
+    public void CommaDelimiterString(string input, int expected)
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add(input);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("1,1,1", 3)]
+    [InlineData("1,2\n2,3,4,5", 17)]
+    public void ArbitraryDelimiterString(string input, int expected)
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add(input);
+
+        Assert.Equal(expected, result);
+    }
+}
